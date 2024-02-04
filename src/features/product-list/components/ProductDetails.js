a9e19ebde6d3ef2aa9be375467afc798ee41966d
Aishjahan <aishjahankhan7376@gmail.com>
2024-02-04 23:37:57 +0530

    cart api

--- src/features/product-list/components/ProductDetails.js
+++ src/features/product-list/components/ProductDetails.js
@@ -41,16 +41,19 @@ export default function ProductDetails() {
   const user = useSelector(selectLoggedInUser);
   const dispatch = useDispatch();
   const params = useParams();
-  const items = useSelector(selectItems)
+  const items = useSelector(selectItems);
 
   const handleCart = (e) => {
     e.preventDefault();
-    if(items.findIndex(item=>item.productId === product.id)<=0){
-      const newItem = { ...product,productId : product.id, quantity: 1, user: user.id };
-    delete newItem["id"];
-    dispatch(addToCartAsync(newItem));
+    if (items.findIndex((item) => item.product.id === product.id) <= 0) {
+      const newItem = {
+        product: product.id,
+        quantity: 1,
+        user: user.id,
+      };
+      dispatch(addToCartAsync(newItem));
     }
-    console.log('already added');
+    console.log("already added");
   };
 
   useEffect(() => {
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -7,13 +7,13 @@ import {
 } from "../features/cart/cartSlice";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { updateUserAsync } from "../features/auth/authSlice";
 import {
-  updateUserAsync,
-} from "../features/auth/authSlice";
-import { createOrderAsync, selectCurrentOrder } from "../features/order/orderSlice";
+  createOrderAsync,
+  selectCurrentOrder,
+} from "../features/order/orderSlice";
 import { selectUserInfo } from "../features/user/userSlice";
 import { discountedPrice } from "../app/constants";
-
 
 function Checkout() {
   const {
@@ -26,9 +26,9 @@ function Checkout() {
   const dispatch = useDispatch();
   const user = useSelector(selectUserInfo);
   const items = useSelector(selectItems);
-  const currentOrder = useSelector(selectCurrentOrder)
+  const currentOrder = useSelector(selectCurrentOrder);
   const totalAmount = items.reduce(
-    (amount, item) => discountedPrice(item) * item.quantity + amount,
+    (amount, item) => discountedPrice(item.product) * item.quantity + amount,
     0
   );
   const totalItems = items.reduce((total, item) => item.quantity + total, 0);
@@ -37,7 +37,7 @@ function Checkout() {
   const [paymentMethod, setPaymentMethod] = useState("cash");
 
   const handleQuantity = (e, item) => {
-    dispatch(updateCartAsync({ ...item, quantity: +e.target.value }));
+    dispatch(updateCartAsync({ id:item.id, quantity: +e.target.value }));
   };
 
   const handleRemove = (e, id) => {
@@ -62,24 +62,28 @@ function Checkout() {
         user,
         paymentMethod,
         selectedAddress,
-        status: 'pending' // other status can be delivered, received.
+        status: "pending", // other status can be delivered, received.
       };
       dispatch(createOrderAsync(order));
       // need to redirect from here to a new page of order success.
     } else {
       // TODO : we can use proper messaging popup here
-      alert('Enter Address and Payment method')
+      alert("Enter Address and Payment method");
     }
     //TODO : Redirect to order-success page
     //TODO : clear cart after order
     //TODO : on server change the stock number of items
   };
 
-
   return (
     <>
       {!items.length && <Navigate to="/" replace={true}></Navigate>}
-      {currentOrder && <Navigate to={`/order-success/${currentOrder.id}`} replace={true}></Navigate>}
+      {currentOrder && (
+        <Navigate
+          to={`/order-success/${currentOrder.id}`}
+          replace={true}
+        ></Navigate>
+      )}
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
           <div className="lg:col-span-3">
@@ -349,8 +353,8 @@ function Checkout() {
                       <li key={item.id} className="flex py-6">
                         <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                           <img
-                            src={item.thumbnail}
-                            alt={item.title}
+                            src={item.product.thumbnail}
+                            alt={item.product.title}
                             className="h-full w-full object-cover object-center"
                           />
                         </div>
@@ -359,12 +363,12 @@ function Checkout() {
                           <div>
                             <div className="flex justify-between text-base font-medium text-gray-900">
                               <h3>
-                                <a href={item.href}>{item.title}</a>
+                                <a href={item.product.href}>{item.product.title}</a>
                               </h3>
-                              <p className="ml-4">${discountedPrice(item)}</p>
+                              <p className="ml-4">${discountedPrice(item.product)}</p>
                             </div>
                             <p className="mt-1 text-sm text-gray-500">
-                              {item.brand}
+                              {item.product.brand}
                             </p>
                           </div>
                           <div className="flex flex-1 items-end justify-between text-sm">
